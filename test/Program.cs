a37541a57800d135ef95d10ef361e8d008c65b14
friedric <friedric@e5482c99-4a0f-0410-8b6a-bb65b18b6982>
2008-06-01 09:51:52 +0000

    Adding a test to duplicate the openssl aes decrypting app which makes use of the new CipherContext.BytesToKey()
    git-svn-id: https://openssl-net.svn.sourceforge.net/svnroot/openssl-net/trunk@51 e5482c99-4a0f-0410-8b6a-bb65b18b6982

--- test/Program.cs
+++ test/Program.cs
@@ -80,6 +80,7 @@ namespace test
 			tests.Add("rsa", new TestRSA());
 			tests.Add("rand", new TestRandom());
 			tests.Add("x509", new TestX509());
+			tests.Add("aes", new TestAES());
 
 			AddNullCommand(tests, "bf");
 			AddNullCommand(tests, "bn");
