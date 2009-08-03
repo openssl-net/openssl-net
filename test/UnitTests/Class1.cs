14517697b073af24504e2e6293a222e74d063c7e
friedric <friedric@e5482c99-4a0f-0410-8b6a-bb65b18b6982>
2009-08-03 11:52:25 +0000

    Starting to add nunit tests, forgot to move random
    git-svn-id: https://openssl-net.svn.sourceforge.net/svnroot/openssl-net/trunk@85 e5482c99-4a0f-0410-8b6a-bb65b18b6982

---
+++ test/UnitTests/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTests.OpenSSL
+{
+	[TestFixture]
+	public class X509CertificateTest
+	{
+		public X509CertificateTest() { }
+
+		[SetUp]
+		void SetUp() 
+		{ }
+		
+		[TearDown]
+		void TearDown() 
+		{ }
+
+		[Test]
+		public void TestSomething()
+		{
+		}
+	}
+}
