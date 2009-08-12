1ed258ab2861a174d43f7f6e75e1c85416fc7e71
Frank Laub <frank.laub@gmail.com>
2009-08-12 03:26:42 -0700

    Moving MemoryTracker out to OpenSSL.Core so it can be reused by unit tests and end users

---
+++ ManagedOpenSsl/Core/MemoryTracker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace OpenSSL.Core
+{
+	/// <summary>
+	/// Useful for tracking down memory leaks
+	/// </summary>
+	public class MemoryTracker
+	{
+		private static int leaked = 0;
+
+		/// <summary>
+		/// Returns the number of bytes leaked between Start() and Finish()
+		/// </summary>
+		public static int Leaked { get { return leaked; } }
+
+		/// <summary>
+		/// Begins memory tracking
+		/// </summary>
+		public static void Start()
+		{
+			leaked = 0;
+			Crypto.MallocDebugInit();
+			Crypto.SetDebugOptions(DebugOptions.All);
+			Crypto.SetMemoryCheck(MemoryCheck.On);
+		}
+
+		/// <summary>
+		/// Stops memory tracking and reports any leaks found since Start() was called.
+		/// </summary>
+		public static void Finish()
+		{
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			GC.Collect();
+
+			Crypto.Cleanup();
+			Crypto.RemoveState(0);
+
+			Crypto.CheckMemoryLeaks(OnMemoryLeak);
+			if (leaked > 0)
+				Console.WriteLine("Leaked total bytes: {0}", leaked);
+
+			Crypto.SetMemoryCheck(MemoryCheck.Off);
+		}
+
+		private static void OnMemoryLeak(uint order, string file, int line, int num_bytes, IntPtr addr)
+		{
+			Console.WriteLine("[{0}] file: {1} line: {2} bytes: {3}", order, file, line, num_bytes);
+			leaked += num_bytes;
+
+			Native.CRYPTO_free(addr);
+		}
+	}
+}
