fd45d3a8e5e0e4789f8521f015ea4b76ec1dc002
Frank Laub <frank.laub@gmail.com>
2012-03-29 01:19:52 -0700

    removing cruft

--- release.cmd
+++ release.cmd
@@ -7,7 +7,6 @@ copy %SRC%\ManagedOpenSsl.dll %TGT%
 copy %SRC%\ManagedOpenSsl.xml %TGT%
 copy %SRC%\libeay32.dll %TGT%
 copy %SRC%\ssleay32.dll %TGT%
-copy %SRC%\test.exe %TGT%
 copy "%SRC%\openssl+.exe" %TGT%
 
 copy "COPYING" %TGT%
