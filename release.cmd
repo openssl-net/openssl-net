c27df2241f1a5267f1624dc5b85401d0d9f462bb
Eugene Bekker <ebekker@powervision.com>
2015-11-29 09:19:07 -0500

    Updating release batch for 64-bit support
    Adding smarts for supporting both 32-bit and 64-bit support

--- release.cmd
+++ release.cmd
@@ -1,16 +1,44 @@
-set SRC=bin\Release
-set TGT=dist\tmp
 
-rmdir /s /q %TGT%
-mkdir %TGT%
-copy %SRC%\ManagedOpenSsl.dll %TGT%
-copy %SRC%\ManagedOpenSsl.xml %TGT%
-copy %SRC%\libeay32.dll %TGT%
-copy %SRC%\ssleay32.dll %TGT%
-copy "%SRC%\openssl+.exe" %TGT%
+@SETLOCAL
+@SET THIS=%0
+@SET THIS_DIR=%~dp0
 
-copy "COPYING" %TGT%
-copy "README" %TGT%
-copy "INSTALL" %TGT%
-copy "LICENSE" %TGT%
-copy "CHANGES" %TGT%
+@REM Package up the 32-bit version
+@SET SRC=%THIS_DIR%bin\x32\Release
+@SET NTV=x86
+@SET TGT=%THIS_DIR%dist\tmp32
+@CALL :sub_copy
+
+@REM Package up the 64-bit version
+@SET SRC=%THIS_DIR%bin\x64\Release
+@SET NTV=x64
+@SET TGT=%THIS_DIR%dist\tmp64
+@CALL :sub_copy
+
+@REM Goto the end of the script
+@GOTO :eof
+
+
+:sub_copy
+@ECHO "Bundling %SRC%:
+
+@IF EXIST %TGT% @RMDIR /s /q %TGT%
+@MKDIR %TGT%
+@MKDIR %TGT%\%NTV%
+
+@COPY "%SRC%\ManagedOpenSsl*.dll"  %TGT%
+@COPY "%SRC%\ManagedOpenSsl*.xml"  %TGT%
+@COPY "%SRC%\openssl*+.exe"        %TGT%
+@COPY "%SRC%\%NTV%\libeay32*.dll"  %TGT%\%NTV%
+@COPY "%SRC%\%NTV%\ssleay32*.dll"  %TGT%\%NTV%
+
+@COPY "%THIS_DIR%COPYING*"  "%TGT%"
+@COPY "%THIS_DIR%README*"   "%TGT%"
+@COPY "%THIS_DIR%INSTALL*"  "%TGT%"
+@COPY "%THIS_DIR%LICENSE*"  "%TGT%"
+@COPY "%THIS_DIR%CHANGES*"  "%TGT%"
+
+@ECHO DONE
+@ECHO.
+@REM Return to the caller
+@GOTO :eof
